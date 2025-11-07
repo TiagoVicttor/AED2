08e0f9a4e45b26cf00c50f888405a0543cb5739b
Victtorazzi <tiagovicttorazzi@gmail.com>
2025-11-07 11:45:08 -0300

    Exercicio 02

---
+++ Lista_6/ex02.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n,m;
+	cout << "Crianças: ";
+	cin >> n;
+	cout << "Doces: ";
+	cin >> m;
+	
+	vector<int> necessidades(n);
+	vector<int> doces(m);
+	
+	cout << "Necessidades das crianças: ";
+	for(int i = 0; i < n; i++){
+		cin >> necessidades[i];
+	}
+	
+	cout << "Valores calóricos dos doces: ";
+	for(int j = 0; j < m; j++){
+		cin >> doces[j];
+	}
+	
+	sort(necessidades.begin(), necessidades.end());
+	sort(doces.begin(), doces.end());
+	
+	int i = 0, j = 0;
+	int satisfeitas = 0;
+	
+	while(i < n && j < m){
+		if(doces[j] >= necessidades[i]){
+			satisfeitas++;
+			i++;
+			j++;
+		}else{
+			j++;
+		}
+	}
+	
+	cout << "Crianças satisfeitas: " << satisfeitas << endl;
+	return 0;
+}
