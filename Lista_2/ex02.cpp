64e611fa1f67ee0929365a49e2f4e624d9e4bab2
Victtorazzi <tiagovicttorazzi@gmail.com>
2025-12-01 20:33:10 -0300

    Update ex02.cpp

--- Lista_2/ex02.cpp
+++ Lista_2/ex02.cpp
@@ -2,39 +2,41 @@
 #include <vector>
 using namespace std;
 
-long long mergeCount(vector<int>& v, int l, int m, int r) {
-    vector<int> left(v.begin() + l, v.begin() + m + 1);
-    vector<int> right(v.begin() + m + 1, v.begin() + r + 1);
+// Função que mescla duas metades do vetor e conta inversões
+long long mesclarContar(vector<int>& v, int esq, int meio, int dir) {
+    vector<int> esquerda(v.begin() + esq, v.begin() + meio + 1);
+    vector<int> direita(v.begin() + meio + 1, v.begin() + dir + 1);
 
-    int i = 0, j = 0, k = l;
-    long long inv = 0;
+    int i = 0, j = 0, k = esq;
+    long long inversoes = 0;
 
-    while (i < left.size() && j < right.size()) {
-        if (left[i] <= right[j]) {
-            v[k++] = left[i++];
+    while (i < esquerda.size() && j < direita.size()) {
+        if (esquerda[i] <= direita[j]) {
+            v[k++] = esquerda[i++];
         } else {
-            v[k++] = right[j++];
-            inv += (left.size() - i); // elementos restantes são inversões
+            v[k++] = direita[j++];
+            inversoes += (esquerda.size() - i); // elementos restantes formam inversões
         }
     }
 
-    while (i < left.size()) v[k++] = left[i++];
-    while (j < right.size()) v[k++] = right[j++];
+    while (i < esquerda.size()) v[k++] = esquerda[i++];
+    while (j < direita.size()) v[k++] = direita[j++];
 
-    return inv;
+    return inversoes;
 }
 
-long long countInv(vector<int>& v, int l, int r) {
-    if (l >= r) return 0;
+// Função principal que divide o vetor e conta inversões usando Merge Sort
+long long contarInversoes(vector<int>& v, int esq, int dir) {
+    if (esq >= dir) return 0;
 
-    int m = (l + r) / 2;
-    return countInv(v, l, m)
-         + countInv(v, m + 1, r)
-         + mergeCount(v, l, m, r);
+    int meio = (esq + dir) / 2;
+    return contarInversoes(v, esq, meio)
+         + contarInversoes(v, meio + 1, dir)
+         + mesclarContar(v, esq, meio, dir);
 }
 
 int main() {
     vector<int> A = {2, 4, 1, 3, 5};
-    cout << countInv(A, 0, A.size() - 1) << endl;
+    cout << contarInversoes(A, 0, A.size() - 1) << endl;
     return 0;
 }
