4ffbed02f54bd66c844073d91f7d8a97c87187fd
Victtorazzi <tiagovicttorazzi@gmail.com>
2025-12-01 20:48:05 -0300

    Update ex01.cpp
    Refactor the sequence search algorithm to use backtracking instead of a separate function. Update main function to allow user input for pyramid values and target sum.

--- Lista_6/ex01.cpp
+++ Lista_6/ex01.cpp
@@ -2,51 +2,77 @@
 #include <vector>
 using namespace std;
 
-void buscarSequencias(const vector<vector<int>>& piramide, int i, int j, int somaAtual, int S, int vector<int>& caminho, vector<vector<int>>& solucoes){
-	if (i>= (int)piramide.size() || j>= (int)piramide[i].size()){
-		return;
-	}
-	somaAtual += piramide[i][j];
-	caminho.push_back(piramide[i][j]);
-	
-	if(somaAtual > S){
-		caminho.pop_back();
-		return;
-	} 
-	
-	if (i == (int)piramide.size() - 1) {
-        	if (somaAtual == S){
-            		solucoes.push_back(caminho);
-    		}
-    	}else{
-    		 buscarSequencias(piramide, i + 1, j, somaAtual, S, caminho, solucoes);
-        	buscarSequencias(piramide, i + 1, j + 1, somaAtual, S, caminho, solucoes);
+vector<vector<int>> piramide;
+vector<vector<bool>> visitado;
+vector<int> caminho;
+vector<vector<int>> solucoes;
+int S;
+
+void backtrack(int i, int j, int soma) {
+    int linhas = piramide.size();
+
+    if (i >= linhas || j < 0 || j >= piramide[i].size()) 
+        return;
+
+    if (visitado[i][j]) 
+        return;
+
+    int valor = piramide[i][j];
+
+    if (soma + valor > S) 
+        return; // poda
+
+    // marca
+    visitado[i][j] = true;
+    caminho.push_back(valor);
+
+    // chegamos no nível final
+    if (i == linhas - 1) {
+        if (soma + valor == S) {
+            solucoes.push_back(caminho);
         }
-        
-        caminho.pop_back();
+    } else {
+        // desce para esquerda e direita
+        backtrack(i + 1, j, soma + valor);
+        backtrack(i + 1, j + 1, soma + valor);
+    }
+
+    // desfaz
+    caminho.pop_back();
+    visitado[i][j] = false;
 }
 
-int main(){
-	vector<vector<int>> piramide {{5},{3,8},{2,4,6},{1,2,7,3}};
-	
-	int S = 16;
-	
-	vector<vector<int>> solucoes;
-	vector<int> caminho;
-	
-	buscaSequencias(piramide,0,0,0,S,caminho,solucoes);
-	
-	if(solucoes.empty()){
-		cout << "Nenhuma solucao emcontrada" << endln;
-	} else {
-		cout << "Solucoes encontradas:\n";
-		for (auto& seq : solucoes){
-			cout << "[";
-			for (int v : seq){
-				cout << v << " ";
-			}
-			cout << "]\n";
-		}
-	}
-	return 0;
+int main() {
+    int n;
+    cout << "Quantidade de linhas da piramide: ";
+    cin >> n;
+
+    piramide.resize(n);
+    visitado.resize(n);
+
+    cout << "Digite os valores da piramide:\n";
+    for (int i = 0; i < n; i++) {
+        piramide[i].resize(i + 1);
+        visitado[i].resize(i + 1, false);
+        for (int j = 0; j < i + 1; j++) {
+            cin >> piramide[i][j];
+        }
+    }
+
+    cout << "Digite o valor da chave secreta S: ";
+    cin >> S;
+
+    backtrack(0, 0, 0);
+
+    if (solucoes.empty()) {
+        cout << "Nenhuma solucao\n";
+    } else {
+        cout << "\nSolucoes encontradas:\n";
+        for (auto &c : solucoes) {
+            for (int x : c) cout << x << " ";
+            cout << endl;
+        }
+    }
+
+    return 0;
 }
